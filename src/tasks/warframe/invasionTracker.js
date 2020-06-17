0d19b597105b333fd264c3dc77362c64e79f68bb
André <andre_20113meira@hotmail.com>
2020-06-17 10:19:28 -0300

    Added try catch to ommit axios errors

--- src/tasks/warframe/invasionTracker.js
+++ src/tasks/warframe/invasionTracker.js
@@ -13,27 +13,33 @@ module.exports = class extends Task {
 
   async init() {
     const runner = async () => {
-      axios.get(invasionUrl).then(async ({ data: invasionsData }) => {
-        const provider = await this.client.providers.get('mongoose');
-        const invasionDocument = await provider.warframe.invasion;
-        const invasionsIds = await invasionDocument.get('data.cacheIds') || [];
-        const needUpdate = invasionsData.reduce((boolean, invasion) => {
-          if (!invasionsIds.includes(invasion.id) && !invasion.completed) {
-            this.client.emit('warframeNewInvasion', invasion);
-            return true;
+      try {
+        axios.get(invasionUrl).then(async ({ data: invasionsData }) => {
+          const provider = await this.client.providers.get('mongoose');
+          const invasionDocument = await provider.warframe.invasion;
+          const invasionsIds = await invasionDocument.get('data.cacheIds') || [];
+          const needUpdate = invasionsData.reduce((boolean, invasion) => {
+            if (!invasionsIds.includes(invasion.id) && !invasion.completed) {
+              this.client.emit('warframeNewInvasion', invasion);
+              return true;
+            }
+            return boolean;
+          }, false);
+          if (needUpdate) {
+            const updatedArr = invasionsData.map((invasion) => invasion.id);
+            if (!invasionDocument.data) {
+              invasionDocument.data = { cacheIds: updatedArr };
+            } else {
+              invasionDocument.data.cacheIds = updatedArr;
+            }
+            console.log(updatedArr);
+            await invasionDocument.updateOne({ 'data.cacheIds': updatedArr });
           }
-          return boolean;
-        }, false);
-        if (needUpdate) {
-          const updatedArr = invasionsData.map((invasion) => invasion.id);
-          if (!invasionDocument.data) {
-            invasionDocument.data = { cacheIds: updatedArr };
-          } else {
-            invasionDocument.data.cacheIds = updatedArr;
-          }
-          await invasionDocument.save();
-        }
-      });
+        });
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.warn(error);
+      }
     };
     setInterval(runner, 5000);
   }
