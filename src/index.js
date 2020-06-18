2d09af7bb67eda26e3b075ba45fa69bb3566f1ca
André <andre_20113meira@hotmail.com>
2020-06-18 11:09:43 -0300

    Permission level added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client } = require('klasa');
+const { Client, PermissionLevels } = require('klasa');
 const { config, token } = require('./config');
 
 class MyKlasaClient extends Client {
@@ -11,5 +11,14 @@ class MyKlasaClient extends Client {
   }
   // Add any methods to your Klasa Client
 }
+
+config.permissionLevels = new PermissionLevels()
+  .add(0, () => true)
+  .add(5, ({ guild, member }) => guild && member.permissions.has('MANAGE_GUILD'), { break: true, fetch: true })
+  .add(6, ({ guild, member }) => guild && member.permissions.has('ADMINISTRATOR'), { break: true, fetch: true })
+  .add(7, ({ guild, member }) => guild && member === guild.owner, { break: true, fetch: true })
+  .add(9, ({ author, client }) => client.owners.has(author), { break: true })
+  .add(10, ({ author, client }) => client.owners.has(author));
+// Allows the bot owner to use Bot Owner only commands, which silently fail for other users.
 
 new MyKlasaClient(config).login(token);
