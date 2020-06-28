ca4327a18d6f343a53b661939c2240699003da6b
André <andre_20113meira@hotmail.com>
2020-06-28 19:48:18 -0300

    Audio recorder now not using lodash

--- src/customExtendables/audioRecorder.js
+++ src/customExtendables/audioRecorder.js
@@ -1,14 +1,14 @@
+/* eslint-disable max-classes-per-file */
 const { Readable } = require('stream');
 const AudioMixer = require('audio-mixer');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
-const _ = require('lodash');
 const { config } = require('../config');
 
 ffmpeg.setFfmpegPath(require('@ffmpeg-installer/ffmpeg').path);
 ffmpeg.setFfprobePath(require('@ffprobe-installer/ffprobe').path);
 
-const audioName = () => {
+const audioDate = () => {
   const startDate = new Date();
   const { language, timezone } = config;
   const options = {
@@ -17,13 +17,17 @@ const audioName = () => {
     fractionalSecondDigits: '2',
     timeZone: timezone,
   };
+  const invalidCaractersReplace = (stringDate) => stringDate
+    .replace(' ', 'T')
+    .replace(/:/g, '_')
+    .replace(/ /g, '');
   const dateFormater = new Intl.DateTimeFormat(language, options);
-  return () => {
-    const endingDate = new Date();
-    return dateFormater.formatRange(startDate, endingDate)
-      .replace(' ', 'T')
-      .replace(/:/g, '_')
-      .replace(/ /g, '');
+  return {
+    startToEndDate: () => {
+      const endingDate = new Date();
+      return invalidCaractersReplace(dateFormater.formatRange(startDate, endingDate));
+    },
+    startDate: invalidCaractersReplace(dateFormater.format(startDate)),
   };
 };
 const silenceFrame = Buffer.from([0xF8, 0xFF, 0xFE]);
@@ -45,44 +49,66 @@ const assignVoiceConnection = (voiceConnection, member, pcmMixer) => {
   voiceStream.pipe(standaloneInput);
 };
 
-module.exports = async (voiceConnection, client) => {
-  const { channel } = voiceConnection;
-  const idPath = `.\\audios\\${_.uniqueId('audio_')}.ogg`;
-  const outputStream = fs.createWriteStream(idPath);
-  const fileRename = (() => {
-    const fileName = audioName();
-    return async () => {
-      await fs.promises.rename(idPath, `.\\audios\\${fileName()}.ogg`);
-    };
-  })();
-  const pcmMixer = new AudioMixer.Mixer({
-    ...voiceInputOptions,
-    clearInterval: 250,
-  });
-  voiceConnection.play(new Silence(), { type: 'opus' });
-  channel.members.array().forEach((member, i) => {
-    const voiceStream = voiceConnection.receiver.createStream(member.user, { mode: 'pcm', end: 'manual' });
-    if (i === 0) {
-      const mixerInput = pcmMixer.input({ ...voiceInputOptions, volume: 100 });
-      return voiceStream.pipe(mixerInput);
+module.exports = class AudioRecorder {
+  constructor(voiceConnection, client) {
+    const audioDateObject = audioDate();
+    this.voiceConnection = voiceConnection;
+    this.client = client;
+    this.channel = voiceConnection.channel;
+    const idPath = `.\\audios\\${audioDateObject.startDate}.ogg`;
+    this.outputStream = fs.createWriteStream(idPath);
+    this.fileRename = (() => {
+      const { startToEndDate } = audioDateObject;
+      return async () => {
+        await fs.promises.rename(idPath, `.\\audios\\${startToEndDate()}.ogg`);
+      };
+    })();
+    this.pcmMixer = new AudioMixer.Mixer({
+      ...voiceInputOptions,
+      clearInterval: 100,
+    });
+    this.isRecording = false;
+  }
+
+  async startRecording() {
+    if (this.isRecording) {
+      await this.stopRecording();
     }
-    return assignVoiceConnection(voiceConnection, member, pcmMixer);
-  });
-  const memberJoinEventPath = `${voiceConnection.channel.id}memberJoined`;
-  client.on(memberJoinEventPath, async (member) => {
-    if (member.guild.id !== voiceConnection.channel.guild.id) return;
-    assignVoiceConnection(voiceConnection, member, pcmMixer);
-  });
-  ffmpeg(pcmMixer)
-    .inputOptions(['-f s16le', '-acodec pcm_s16le', '-ac 2', '-ar 48000'])
-    .audioQuality(24)
-    .audioChannels(1)
-    .audioCodec('opus')
-    .format('opus')
-    .on('error', client.console.error)
-    .on('end', fileRename)
-    .pipe(outputStream);
-  voiceConnection.on('endRecording', async () => {
-    pcmMixer.emit('end');
-  });
+    this.isRecording = true;
+    const {
+      voiceConnection, client, channel, outputStream, fileRename, pcmMixer,
+    } = this;
+    voiceConnection.play(new Silence(), { type: 'opus' });
+    channel.members.array().forEach((member, i) => {
+      const voiceStream = voiceConnection.receiver.createStream(member.user, { mode: 'pcm', end: 'manual' });
+      if (i === 0) {
+        const mixerInput = pcmMixer.input({ ...voiceInputOptions, volume: 100 });
+        return voiceStream.pipe(mixerInput);
+      }
+      return assignVoiceConnection(voiceConnection, member, pcmMixer);
+    });
+    const memberJoinEventPath = `${voiceConnection.channel.id}memberJoined`;
+    client.on(memberJoinEventPath, async (member) => {
+      if (member.guild.id !== voiceConnection.channel.guild.id) return;
+      assignVoiceConnection(voiceConnection, member, pcmMixer);
+    });
+    ffmpeg(pcmMixer)
+      .inputOptions(['-f s16le', '-acodec pcm_s16le', '-ac 2', '-ar 48000'])
+      .audioQuality(24)
+      .audioChannels(1)
+      .audioCodec('opus')
+      .format('opus')
+      .on('error', client.console.error)
+      .on('end', fileRename)
+      .pipe(outputStream);
+    voiceConnection.on('disconnect', async () => this.stopRecording());
+  }
+
+  async stopRecording() {
+    this.pcmMixer.emit('end');
+    this.pcmMixer.close();
+    this.pcmMixer.removeAllListeners();
+    this.pcmMixer.destroy();
+    this.isRecording = false;
+  }
 };
