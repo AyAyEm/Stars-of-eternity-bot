12db9f871b384492cfdb91e6997b2afa9d4685dd
André <andre_20113meira@hotmail.com>
2020-06-29 17:25:42 -0300

    Minor change

---
+++ src/events/parsers/voiceStateParser.js
@@ -0,0 +1,39 @@
+/* eslint-disable max-classes-per-file */
+const { Event } = require('klasa');
+
+module.exports = class extends Event {
+  constructor(...args) {
+    super(...args, {
+      event: 'voiceStateUpdate',
+      enabled: true,
+      once: false,
+    });
+  }
+
+  async run(oldState, newState) {
+    const oldChannelID = oldState.channelID;
+    const newChannelID = newState.channelID;
+    const bothDefined = oldChannelID && newChannelID;
+    if (oldChannelID === newChannelID) return;
+    // Member joined a channel
+    if (!oldChannelID || (oldChannelID !== newChannelID && bothDefined)) {
+      const { member, channel } = newState;
+      const botOrMember = member.user.bot ? 'bot' : 'member';
+      this.client.emit(`${botOrMember}JoinedChannel`, {
+        member, channel, state: newState, type: 'join',
+      });
+      // Custom events
+      if (!member.user.bot) this.client.emit(`${channel.id}memberJoined`, member);
+    }
+    // Member left a channel
+    if (!newChannelID || (oldChannelID !== newChannelID && bothDefined)) {
+      const { member, channel } = oldState;
+      const botOrMember = member.user.bot ? 'bot' : 'member';
+      this.client.emit(`${botOrMember}LeftChannel`, {
+        member, channel, state: oldState, type: 'left',
+      });
+      // Custom events
+      if (!member.user.bot) this.client.emit(`${channel.id}memberLeft`, member);
+    }
+  }
+};
