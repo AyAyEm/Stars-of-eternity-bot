de10ed085148de75a2e4fbbf141c0de9173c07e8
André <andre_20113meira@hotmail.com>
2020-07-01 20:25:17 -0300

    Disabled bot following members for now

--- src/events/botRecording/botFollowMembers.js
+++ src/events/botRecording/botFollowMembers.js
@@ -3,7 +3,7 @@ const { Event } = require('klasa');
 module.exports = class extends Event {
   constructor(...args) {
     super(...args, {
-      enabled: true,
+      enabled: false,
       once: false,
     });
   }
