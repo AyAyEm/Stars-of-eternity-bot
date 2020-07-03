437211216c741a5f5ca0d97658b317ccb88c2443
André <andre_20113meira@hotmail.com>
2020-07-03 18:59:34 -0300

    fissureTracker embed

---
+++ src/embeds/warframe/fissureTracker.js
@@ -0,0 +1,29 @@
+const { RichEmbed } = require('discord.js');
+
+// // Fissures: Lith, Meso, Neo, Axi and Requiem
+const fissureIcons = [
+  'https://i.imgur.com/ZSxJCTI.png',
+  'https://i.imgur.com/JR0s2vZ.png',
+  'https://i.imgur.com/JNv2xcR.png',
+  'https://i.imgur.com/sk2WIeA.png',
+  'https://i.imgur.com/CNdPs70.png',
+];
+
+const fissuresEmbed = (fissures) => {
+  // const { id, tier, tierNum, activation, eta, missionType, node } = fissures[0];
+  const missionsTypes = fissures.reduce((types, { missionType }, index) => `${types} ${missionType}${index === fissures.length - 1 ? '' : ','}`, '');
+  const { tierNum, tier } = fissures[0];
+  const fields = fissures.map(({ missionType, node }) => ({ name: missionType, value: node }));
+  const embed = new RichEmbed()
+    .setTitle(`${tier} fendas ativas: ${fissures.length}`)
+    .setAuthor(missionsTypes)
+    .setDescription('')
+    .setThumbnail(fissureIcons[tierNum - 1])
+    .setTimestamp();
+  fields.forEach((field) => embed.addField(field.name, field.value));
+  return embed;
+};
+
+module.exports = {
+  fissuresEmbed,
+};
