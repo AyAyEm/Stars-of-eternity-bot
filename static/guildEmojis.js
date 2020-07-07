4a0c7419dc81da99dd003c5355c4707625079ee9
André <andre_20113meira@hotmail.com>
2020-07-07 20:19:00 -0300

    guildEmoji static links

---
+++ static/guildEmojis.js
@@ -0,0 +1,11 @@
+const emojis = new Map([
+  // Warframe
+  ['warframeRelics', {
+    lith: { name: 'lithRelic', image: 'https://i.imgur.com/ZSxJCTI.png' },
+    meso: { name: 'mesoRelic', image: 'https://i.imgur.com/JR0s2vZ.png' },
+    neo: { name: 'neoRelic', image: 'https://i.imgur.com/JNv2xcR.png' },
+    axi: { name: 'axiRelic', image: 'https://i.imgur.com/sk2WIeA.png' },
+    requiem: { name: 'requiemRelic', image: 'https://i.imgur.com/CNdPs70.png' },
+  }],
+]);
+module.exports = emojis;
