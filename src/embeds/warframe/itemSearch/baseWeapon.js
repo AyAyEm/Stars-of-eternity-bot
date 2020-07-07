aacd002e7a6451fc46a057606a4c383a82c64c88
André <andre_20113meira@hotmail.com>
2020-07-06 21:17:38 -0300

    Embed to use with other functions

---
+++ src/embeds/warframe/itemSearch/baseWeapon.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js');
+const masteryRankImgs = require('./static/masteryRankImgs');
+
+const baseWeapon = (weapon) => {
+  const {
+    name, type, imageName, category, masteryReq,
+  } = weapon;
+  const embed = new MessageEmbed();
+  embed
+    // .setAuthor(name)
+    .setTitle(name)
+    .addFields([
+      { name: 'Tipo', value: type, inline: true },
+      { name: 'Categoria', value: category, inline: true },
+    ])
+    .setThumbnail(`https://cdn.warframestat.us/img/${imageName}`)
+    .setFooter(`Maestria necessária: ${masteryReq}`, masteryRankImgs[masteryReq]);
+  return embed;
+};
+
+module.exports = baseWeapon;
