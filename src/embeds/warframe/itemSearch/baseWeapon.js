8d80d177a9b8881fb1c4da14650128173cd3b913
André <andre_20113meira@hotmail.com>
2020-07-09 14:07:29 -0300

    Riven disposition added

--- src/embeds/warframe/itemSearch/baseWeapon.js
+++ src/embeds/warframe/itemSearch/baseWeapon.js
@@ -1,17 +1,18 @@
 const { MessageEmbed } = require('discord.js');
 const masteryRankImgs = require('../../../../static/masteryRankImgs');
+const rivenDisposition = require('../../../../static/rivenDisposition');
 
 const baseWeapon = (weapon) => {
   const {
-    name, type, imageName, category, masteryReq,
+    name, type, imageName, category, masteryReq, disposition,
   } = weapon;
   const embed = new MessageEmbed();
   embed
-    // .setAuthor(name)
     .setTitle(name)
+    .setDescription(rivenDisposition[disposition - 1])
     .addFields([
-      { name: 'Tipo', value: type, inline: true },
       { name: 'Categoria', value: category, inline: true },
+      { name: 'Tipo', value: type, inline: true },
     ])
     .setThumbnail(`https://cdn.warframestat.us/img/${imageName}`)
     .setFooter(`Maestria ${masteryReq}`, masteryRankImgs[masteryReq]);
