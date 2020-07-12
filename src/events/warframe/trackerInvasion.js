6b73e1e537ca57134e31f5f421d2bb7c412e6c4c
André <andre_20113meira@hotmail.com>
2020-07-12 20:58:13 -0300

    Updated to use async

--- src/events/warframe/trackerInvasion.js
+++ src/events/warframe/trackerInvasion.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const { Event } = require('klasa');
+const { eachOf } = require('async');
 const invasionEmbed = require('../../embeds/warframe/invasionTracker');
 
 module.exports = class extends Event {
@@ -14,10 +15,9 @@ module.exports = class extends Event {
 
   async run(invasion) {
     this.provider.Guilds.find({}).stream()
-      .on('data', async (data) => {
-        if (!data.channels) return;
-        const channels = new Map([...Object.entries(data.channels)]);
-        channels.forEach(async ({ invasionItems }, channelId) => {
+      .on('data', async ({ channels }) => {
+        if (!channels) return;
+        await eachOf(channels, async ({ invasionItems }, channelId) => {
           if (!invasionItems || !invasionItems.enabled) return;
           const { items: storedItems } = invasionItems;
           if (!storedItems || storedItems.length === 0) return;
