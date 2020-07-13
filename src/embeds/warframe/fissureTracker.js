c653ff621f6bd73309c351beaf8f2a5dd5f151b5
André <andre_20113meira@hotmail.com>
2020-07-13 06:26:07 -0300

    Updated fissureTracker embed to output a map

--- src/embeds/warframe/fissureTracker.js
+++ src/embeds/warframe/fissureTracker.js
@@ -1,4 +1,6 @@
 const { MessageEmbed } = require('discord.js');
+const { groupBy, eachSeries } = require('async');
+const _ = require('lodash');
 
 // // Fissures: Lith, Meso, Neo, Axi and Requiem
 const fissureIcons = [
@@ -8,20 +10,42 @@ const fissureIcons = [
   'https://i.imgur.com/sk2WIeA.png',
   'https://i.imgur.com/CNdPs70.png',
 ];
+const fissureTiers = new Map(['Lith', 'Meso', 'Neo', 'Axi', 'Requiem']
+  .map((tier, index) => [tier, { index, icon: fissureIcons[index] }]));
 
-const fissuresEmbed = (fissures) => {
-  // const { id, tier, tierNum, activation, eta, missionType, node } = fissures[0];
-  const missionsTypes = fissures.reduce((types, { missionType }, index) => `${types} ${missionType}${index === fissures.length - 1 ? '' : ','}`, '');
-  const { tierNum, tier } = fissures[0];
-  const fields = fissures.map(({ missionType, node }) => ({ name: missionType, value: node }));
-  const embed = new MessageEmbed()
-    .setTitle(`${tier} fendas ativas: ${fissures.length}`)
-    .setAuthor(missionsTypes)
-    .setDescription('')
-    .setThumbnail(fissureIcons[tierNum - 1])
-    .setTimestamp();
-  fields.forEach((field) => embed.addField(field.name, field.value));
-  return embed;
+const fissuresEmbed = async (fissures) => {
+  const fissuresMap = new Map(
+    [...Object.entries((await groupBy(fissures, async (fissure) => fissure.tier)))]
+      .sort(([tierA], [tierB]) => {
+        const [{ index: A }, { index: B }] = [fissureTiers.get(tierB), fissureTiers.get(tierA)];
+        return A - B;
+      }),
+  );
+  const embedsMap = new Map();
+  await eachSeries(fissuresMap, async ([tier, tierFissures]) => {
+    const { icon } = fissureTiers.get(tier);
+    const missionsTypes = _.uniqBy(tierFissures, 'missionType')
+      .reduce((types, { missionType }, index, uniqFissures) => (
+        `${types} ${missionType}${index === uniqFissures.length - 1 ? '' : ','}`), '');
+    const fields = Object.values(await groupBy(tierFissures,
+      async ({ missionType }) => missionType))
+      .map(([...missions]) => ({
+        name: missions[0].missionType,
+        value: missions.reduce((nodes, { node }) => {
+          const [mission, map] = [node.split(' ')[0], node.split(' ').slice(1).join(' ')];
+          return `${nodes}***${map.replace(/[()]/g, '')}***    *${mission}*\n`;
+        }, ''),
+      }));
+    const embed = new MessageEmbed()
+      .setTitle(`${tier} fendas ativas: ${tierFissures.length}`)
+      .setAuthor(missionsTypes)
+      .setDescription('')
+      .setThumbnail(icon)
+      .setTimestamp()
+      .addFields(...fields);
+    embedsMap.set(tier, embed);
+  });
+  return embedsMap;
 };
 
 module.exports = fissuresEmbed;
