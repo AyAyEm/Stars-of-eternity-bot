573b2d030e3742ea50f67404e779655785f2384c
André <andre_20113meira@hotmail.com>
2020-07-14 03:44:11 -0300

    Addition of 2 fields faction and expiration

--- src/embeds/warframe/fissureTracker.js
+++ src/embeds/warframe/fissureTracker.js
@@ -1,8 +1,10 @@
 const { MessageEmbed } = require('discord.js');
 const { groupBy, eachSeries } = require('async');
+const moment = require('moment-timezone');
 const _ = require('lodash');
 
-// // Fissures: Lith, Meso, Neo, Axi and Requiem
+const { timezone } = require('../../config').config;
+// Fissures: Lith, Meso, Neo, Axi and Requiem
 const fissureIcons = [
   'https://i.imgur.com/ZSxJCTI.png',
   'https://i.imgur.com/JR0s2vZ.png',
@@ -12,6 +14,33 @@ const fissureIcons = [
 ];
 const fissureTiers = new Map(['Lith', 'Meso', 'Neo', 'Axi', 'Requiem']
   .map((tier, index) => [tier, { index, icon: fissureIcons[index] }]));
+const fissureToString = ({ node }) => {
+  const [mission, map] = [node.split(' ')[0], node.split(' ').slice(1).join(' ')];
+  // const expirationString = moment.tz(expiry, timezone).format('HH:mm');
+  return `***${map.replace(/[()]/g, '')}***    *${mission}*\n`;
+};
+const missionFissuresToString = (missionFissures) => (
+  missionFissures.reduce((nodes, missionFissure) => `${nodes}${fissureToString(missionFissure)}`, ''));
+const getMissionsFields = (missionFissures) => {
+  const expirationString = missionFissures.reduce((expirations, { expiry }) => {
+    const expiration = moment.tz(expiry, timezone).format('HH:mm');
+    return `${expirations}*${expiration}*\n`;
+  }, '');
+  const enemyFactions = missionFissures.reduce((factions, { enemy }) => `${factions}${enemy}\n`, '');
+  return [{
+    name: `${missionFissures[0].missionType}`,
+    value: missionFissuresToString(missionFissures),
+    inline: true,
+  }, {
+    name: 'Facção',
+    value: enemyFactions,
+    inline: true,
+  }, {
+    name: 'Expira ás',
+    value: expirationString,
+    inline: true,
+  }];
+};
 
 const fissuresEmbed = async (fissures) => {
   const fissuresMap = new Map(
@@ -27,21 +56,16 @@ const fissuresEmbed = async (fissures) => {
     const missionsTypes = _.uniqBy(tierFissures, 'missionType')
       .reduce((types, { missionType }, index, uniqFissures) => (
         `${types} ${missionType}${index === uniqFissures.length - 1 ? '' : ','}`), '');
-    const fields = Object.values(await groupBy(tierFissures,
-      async ({ missionType }) => missionType))
-      .map(([...missions]) => ({
-        name: missions[0].missionType,
-        value: missions.reduce((nodes, { node }) => {
-          const [mission, map] = [node.split(' ')[0], node.split(' ').slice(1).join(' ')];
-          return `${nodes}***${map.replace(/[()]/g, '')}***    *${mission}*\n`;
-        }, ''),
-      }));
+    const groupedByType = await groupBy(tierFissures, async ({ missionType }) => missionType);
+    const fields = Object.values(groupedByType)
+      .flatMap(([...missionFissures]) => getMissionsFields(missionFissures));
     const embed = new MessageEmbed()
       .setTitle(`${tier} fendas ativas: ${tierFissures.length}`)
       .setAuthor(missionsTypes)
       .setDescription('')
       .setThumbnail(icon)
       .setTimestamp()
+      .setFooter('Horário de São Paulo')
       .addFields(...fields);
     embedsMap.set(tier, embed);
   });
