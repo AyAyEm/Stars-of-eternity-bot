06d201f1f393695681e405e052ebb7dcdf263d3e
André <andre_20113meira@hotmail.com>
2020-07-20 11:52:25 -0300

    Added isPrime and updated the search method

--- src/embeds/warframe/itemSearch/index.js
+++ src/embeds/warframe/itemSearch/index.js
@@ -1,11 +1,14 @@
-const weapon = require('./weapons/weapon');
-const primeWeapon = require('./weapons/primeWeapon');
+const { weapon, primeWeapon } = require('./weapons');
 
+const isPrime = ({ name }) => name.includes('Prime');
 const typeFunctions = new Map([
-  ['Weapons', (item) => (item.name.includes('Prime') ? primeWeapon(item) : weapon(item))],
+  ['Weapons', (item) => (isPrime(item) ? primeWeapon(item) : weapon(item))],
 ]);
+const typeDictionary = new Map([
+  [['Arch-Gun', 'Arch-Melee', 'Melee', 'Primary', 'Secondary'], 'Weapons'],
+].flatMap(([keys, value]) => keys.map((key) => [key, value])));
 
 module.exports = (item) => {
-  const type = item.uniqueName.split('/')[2];
+  const type = typeDictionary.get(item.category);
   return typeFunctions.get(type)(item);
 };
