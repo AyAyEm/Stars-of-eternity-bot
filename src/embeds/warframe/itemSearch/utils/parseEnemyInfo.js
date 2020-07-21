ee6e493abff7d4e8b2fe72304f54535030dc23b9
André <andre_20113meira@hotmail.com>
2020-07-21 13:51:30 -0300

    Fixed bug for mission drops

--- src/embeds/warframe/itemSearch/utils/parseEnemyInfo.js
+++ src/embeds/warframe/itemSearch/utils/parseEnemyInfo.js
@@ -1,6 +1,6 @@
 module.exports = (enemy) => {
   const { location, chance } = enemy;
   const [name, dropChance] = location.split(' nce: ', 2);
-  const actualChance = Number(dropChance) * chance;
+  const actualChance = dropChance ? Number(dropChance) * chance : chance * 100;
   return [name, actualChance];
 };
