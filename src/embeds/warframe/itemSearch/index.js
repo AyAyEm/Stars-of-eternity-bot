8f83fa091fe9929bb0f0d4d721416967342baea9
André <andre_20113meira@hotmail.com>
2020-07-22 11:54:37 -0300

    Addition of warframes embed

--- src/embeds/warframe/itemSearch/index.js
+++ src/embeds/warframe/itemSearch/index.js
@@ -1,11 +1,14 @@
 const { weapon, primeWeapon } = require('./weapons');
+const { warframe, warframePrime } = require('./warframes');
 
 const isPrime = ({ name }) => name.includes('Prime');
 const typeFunctions = new Map([
   ['Weapons', (item) => (isPrime(item) ? primeWeapon(item) : weapon(item))],
+  ['Warframes', (item) => (isPrime(item) ? warframePrime(item) : warframe(item))],
 ]);
 const typeDictionary = new Map([
   [['Arch-Gun', 'Arch-Melee', 'Melee', 'Primary', 'Secondary'], 'Weapons'],
+  [['Archwing', 'Warframes'], 'Warframes'],
 ].flatMap(([keys, value]) => keys.map((key) => [key, value])));
 
 module.exports = (item) => {
