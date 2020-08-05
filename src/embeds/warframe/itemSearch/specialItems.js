22762c7c522df55a5eb38ef9998c94f8dee69ca2
WarNote <andre_20113meira@hotmail.com>
2020-08-05 05:07:37 -0300

    Added login weapons

--- src/embeds/warframe/itemSearch/specialItems.js
+++ src/embeds/warframe/itemSearch/specialItems.js
@@ -1,5 +1,13 @@
+const loginWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
+  + ' dias a partir do 100\nExemplo:\n[100, 300, 500, 700, 900...]';
+const sigmaWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
++ ' dias a partir do 300\nExemplo:\n[300, 500, 700, 900...]';
+const acquisitionField = (value, inline = false) => (embed) => embed.addField('Aquisição', value, inline);
+
 module.exports = new Map([
-  ['Athodai', (weaponEmbed) => {
-    weaponEmbed.spliceFields(2, 1, { name: 'Aquisição', value: 'TennoCon live 2020', inline: false });
-  }],
+  ['Athodai', acquisitionField('TennoCon live 2020')],
+  ['Azima', acquisitionField(loginWeapon)],
+  ['Zenistar', acquisitionField(loginWeapon)],
+  ['Zenith', acquisitionField(loginWeapon)],
+  ['Sigma & Octantis', acquisitionField(sigmaWeapon)],
 ]);
