7eeeeb551369b8138e07b055701c7169a78143c0
WarNote <andre_20113meira@hotmail.com>
2020-08-05 22:13:01 -0300

    Changed the texts

--- src/embeds/warframe/itemSearch/specialItems.js
+++ src/embeds/warframe/itemSearch/specialItems.js
@@ -1,11 +1,11 @@
 const loginWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
-  + ' dias a partir do 100\nExemplo:\n[100, 300, 500, 700, 900...]';
+  + ' dias a partir do 100\n**Exemplo**:\n*[100, 300, 500, 700, 900...]*';
 const sigmaWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
-+ ' dias a partir do 300\nExemplo:\n[300, 500, 700, 900...]';
++ ' dias a partir do 300\n**Exemplo**:\n*[300, 500, 700, 900...]*';
 const acquisitionField = (value, inline = false) => (embed) => embed.addField('Aquisição', value, inline);
 
 module.exports = new Map([
-  ['Athodai', acquisitionField('TennoCon live 2020')],
+  ['Athodai', acquisitionField('TennoCon 2020\nAssistindo a live por 30 minutos na Twitch')],
   ['Azima', acquisitionField(loginWeapon)],
   ['Zenistar', acquisitionField(loginWeapon)],
   ['Zenith', acquisitionField(loginWeapon)],
