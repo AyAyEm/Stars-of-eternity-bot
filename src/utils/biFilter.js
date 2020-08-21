8edd2e0bef766c5b11c56d7ee94d827098a819d9
WarNote <andre_20113meira@hotmail.com>
2020-08-21 20:49:00 -0300

    Added documentation to it

--- src/utils/biFilter.js
+++ src/utils/biFilter.js
@@ -1,4 +1,16 @@
-module.exports = function biFilter(array, callbackfn, thisArg = this) {
+/**
+ * Filter an array into two arrays.
+ *
+ * Defined by the callbackFn, the first array will be with the truth elements
+ * and the second array will be with the falsey elements.
+ *
+ * @param {Array} array
+ * @param {Function} callbackfn
+ * @param {Object} thisArg
+ * @return {[Array, Array]} A matrix with two arrays.
+ */
+
+function biFilter(array, callbackfn, thisArg = this) {
   const [truthElements, falseyElements] = [[], []];
   for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
@@ -9,4 +21,6 @@ module.exports = function biFilter(array, callbackfn, thisArg = this) {
     }
   }
   return [truthElements, falseyElements];
-};
+}
+
+module.exports = biFilter;
