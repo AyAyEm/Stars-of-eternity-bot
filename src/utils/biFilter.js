8721e2a5bad1041d7cae1c236e33cde0df1d542c
WarNote <andre_20113meira@hotmail.com>
2020-08-21 20:55:48 -0300

    Fixed english errors

--- src/utils/biFilter.js
+++ src/utils/biFilter.js
@@ -1,8 +1,8 @@
 /**
  * Filter an array into two arrays.
  *
- * Defined by the callbackFn, the first array will be with the truth elements
- * and the second array will be with the falsey elements.
+ * Defined by the callbackFn, the first array will be with the true elements
+ * and the second array will be with the false elements.
  *
  * @param {Array} array
  * @param {Function} callbackfn
@@ -11,16 +11,16 @@
  */
 
 function biFilter(array, callbackfn, thisArg = this) {
-  const [truthElements, falseyElements] = [[], []];
+  const [trueElements, falseElements] = [[], []];
   for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
     if (callbackfn.call(thisArg, element, i, array)) {
-      truthElements.push(element);
+      trueElements.push(element);
     } else {
-      falseyElements.push(element);
+      falseElements.push(element);
     }
   }
-  return [truthElements, falseyElements];
+  return [trueElements, falseElements];
 }
 
 module.exports = biFilter;
