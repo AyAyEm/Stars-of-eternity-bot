032ca3a089bc9f8082c29d3f789e40fdb1f6d8e1
WarNote <andre_20113meira@hotmail.com>
2020-08-21 20:48:13 -0300

    Changed the way async is called

--- src/extendables/message/multiReact.js
+++ src/extendables/message/multiReact.js
@@ -1,5 +1,5 @@
 const { Extendable, KlasaMessage } = require('klasa');
-const { until } = require('async');
+const async = require('async');
 
 module.exports = class extends Extendable {
   constructor(...args) {
@@ -13,7 +13,7 @@ module.exports = class extends Extendable {
     let [stop, iteration] = [false, 0];
     const stopReactions = () => { stop = true; };
     const emojisIterator = emojis.values();
-    const reactionLoop = until(async () => stop || iteration >= emojis.length, async () => {
+    const reactionLoop = async.until(async () => stop || iteration >= emojis.length, async () => {
       iteration += 1;
       return (await this.react(emojisIterator.next().value)).fetch();
     });
