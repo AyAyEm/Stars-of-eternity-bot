4766c6f5e2ec2e60a8a3d11d0a4e8bc8a6950065
WarNote <andre_20113meira@hotmail.com>
2020-08-21 20:48:32 -0300

    Fixed a bug in the all property

--- src/static/warframe/itemNames.js
+++ src/static/warframe/itemNames.js
@@ -3,20 +3,24 @@ const items = {
     'nanoSpores', 'rubedo', 'salvage', 'plastids', 'polymerBundle',
     'cryotic', 'circuits', 'ferrite', 'alloyPlate',
   ],
+
   uncommonItems: ['oxium', 'gallium', 'morphics', 'controlModule'],
+
   rareItems: [
     'neuralSensors', 'orokinCell', 'argonCrystal', 'tellurium', 'neurodes', 'nitain',
   ],
+
   weapons: ['vandal', 'wraith', 'weapon'],
   goodOnes: ['reactor', 'catalyst', 'forma', 'aura', 'exilus', 'riven'],
   faction: ['fieldron', 'detonite', 'mutagen'],
+
   others: [
     'mutalist', 'forma', 'synthula', 'kavatGene',
     'kubrowEgg', 'traces', 'other', 'credits', 'skin',
     'helmet', 'nightmare', 'endo',
   ],
-
-  all: Object.values(this),
 };
+
+items.all = Object.values(items).flat();
 
 module.exports = items;
