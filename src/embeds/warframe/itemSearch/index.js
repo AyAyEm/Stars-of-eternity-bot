7fe222c34e99bf0ff78dd352b6b177b0c89968c1
WarNote <andre_20113meira@hotmail.com>
2020-08-27 12:29:30 -0300

    updated for mods item search

--- src/embeds/warframe/itemSearch/index.js
+++ src/embeds/warframe/itemSearch/index.js
@@ -1,15 +1,18 @@
 const { weapon, primeWeapon } = require('./weapons');
 const { warframe, warframePrime } = require('./warframes');
+const mods = require('./mods');
 
 const isPrime = ({ name }) => name.includes('Prime');
 const typeFunctions = new Map([
   ['Weapons', (item) => (isPrime(item) ? primeWeapon(item) : weapon(item))],
   ['Warframes', (item) => (isPrime(item) ? warframePrime(item) : warframe(item))],
+  ['Mods', mods],
 ]);
 
 const typeDictionary = new Map([
   [['Arch-Gun', 'Arch-Melee', 'Melee', 'Primary', 'Secondary'], 'Weapons'],
   [['Archwing', 'Warframes'], 'Warframes'],
+  [['Mods'], 'Mods'],
 ].flatMap(([keys, value]) => keys.map((key) => [key, value])));
 
 module.exports = (item) => {
