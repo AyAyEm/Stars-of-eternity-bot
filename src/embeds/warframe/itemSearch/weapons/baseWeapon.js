b3a0ca5406b88078e52f4bf949ca58c1cab52161
WarNote <andre_20113meira@hotmail.com>
2020-09-01 12:17:08 -0300

    Added weapon utility informations

--- src/embeds/warframe/itemSearch/weapons/baseWeapon.js
+++ src/embeds/warframe/itemSearch/weapons/baseWeapon.js
@@ -32,19 +32,24 @@ class BaseWeapon {
       noise, trigger, magazineSize, reloadTime, multishot, ammo, damage,
       // eslint-disable-next-line no-unused-vars
       damageTypes, totalDamage, flight, projectile, secondary, areaAttack,
+      category,
     } = weapon;
     const embedStrings = {
-      chance: `Chance: ${Math.round(criticalChance * 100)}%\n`
-        + `Multiplicador: ${criticalMultiplier}x`,
+      chance: `Chance: ${Math.round(criticalChance * 100)}%\nMultiplicador: ${criticalMultiplier}x`,
       status: `Chance: ${Math.round(procChance * 100)}%`,
-      dano: `${Object.entries(damageTypes).map(([type, dmg]) => `${type}: ${dmg}`).join('\n')}`,
-      munition: `Pente: ${magazineSize}\n`
-        + `Total: ${ammo}`,
+      damage: `${Object.entries(damageTypes).map(([type, dmg]) => `${type}: ${dmg}`).join('\n')}`,
+      munition: `Pente: ${magazineSize}\nTotal: ${ammo}`,
+      utility: `${category === 'Melee' ? `Velocidade de ataque: ${fireRate.toFixed(3)}` : `Taxa de tiro: ${fireRate}/s`}`
+        + `${trigger ? `\nGatilho: ${trigger}` : ''}`
+        + `${projectile ? `\nProjétil: ${projectile}` : ''}`
+        + `${reloadTime ? `\nRecarga: ${reloadTime}s` : ''}`
+        + `${accuracy ? `\nPrecisão: ${accuracy}` : ''}`,
     };
     const fields = [
-      { name: `Dano ${totalDamage}`, value: embedStrings.dano, inline: false },
+      { name: `Dano ${totalDamage}`, value: embedStrings.damage, inline: false },
       { name: 'Crítico', value: embedStrings.chance, inline: false },
       { name: 'Status', value: embedStrings.status, inline: false },
+      { name: 'Utilidade', value: embedStrings.utility, inline: false },
     ];
     if (ammo) fields.push({ name: 'Munição', value: embedStrings.munition, inline: true });
     baseEmbed.addFields(fields);
