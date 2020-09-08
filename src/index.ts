9a57238c431568b8bca1de248b3c32fb894af7f6
WarNote <andre_20113meira@hotmail.com>
2020-09-08 13:51:52 -0300

    Changed index to typescript

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const { config, token } = require('./config');
 
 class MyKlasaClient extends Client {
   // eslint-disable-next-line no-useless-constructor
-  constructor(...args) {
+  constructor(...args: any) {
     super(...args);
     // Add any properties to your Klasa Client
   }
@@ -14,11 +14,11 @@ class MyKlasaClient extends Client {
 
 config.permissionLevels = new PermissionLevels()
   .add(0, () => true)
-  .add(5, ({ guild, member }) => guild && member.permissions.has('MANAGE_GUILD'), { break: true, fetch: true })
-  .add(6, ({ guild, member }) => guild && member.permissions.has('ADMINISTRATOR'), { break: true, fetch: true })
-  .add(7, ({ guild, member }) => guild && member === guild.owner, { break: true, fetch: true })
-  .add(9, ({ author, client }) => client.owners.has(author), { break: true })
-  .add(10, ({ author, client }) => client.owners.has(author));
+  .add(5, ({ guild, member }: any) => guild && member.permissions.has('MANAGE_GUILD'), { break: true, fetch: true })
+  .add(6, ({ guild, member }: any) => guild && member.permissions.has('ADMINISTRATOR'), { break: true, fetch: true })
+  .add(7, ({ guild, member }: any) => guild && member === guild.owner, { break: true, fetch: true })
+  .add(9, ({ author, client }: any) => client.owners.has(author), { break: true })
+  .add(10, ({ author, client }: any) => client.owners.has(author));
 // Allows the bot owner to use Bot Owner only commands, which silently fail for other users.
 
 new MyKlasaClient(config).login(token);
