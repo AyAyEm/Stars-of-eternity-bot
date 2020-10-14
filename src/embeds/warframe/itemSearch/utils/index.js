075f03a1b3024d11ba7b539bb99ce27286623731
WarNote <andre_20113meira@hotmail.com>
2020-10-14 16:17:30 -0300

    Updated to typescript

--- src/embeds/warframe/itemSearch/utils/index.js
+++
@@ -1,5 +0,0 @@
-const blueprintSource = require('./blueprintsSource');
-const dropToNameAndChance = require('./dropToNameAndChance');
-const relicDrop = require('./relicDrop');
-
-module.exports = { blueprintSource, dropToNameAndChance, relicDrop };
