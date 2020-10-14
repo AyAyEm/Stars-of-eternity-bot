78c9ed425d89ddec97b32be149d3e875236cca81
WarNote <andre_20113meira@hotmail.com>
2020-10-14 18:13:18 -0300

    Updated to typescript

--- src/embeds/warframe/itemSearch/specialItems.ts
+++ src/embeds/warframe/itemSearch/specialItems.ts
@@ -1,10 +1,15 @@
+import type { MessageEmbed } from 'discord.js';
+
 const loginWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
   + ' dias a partir do 100\n**Exemplo**:\n*[100, 300, 500, 700, 900...]*';
 const sigmaWeapon = 'Arma de login\nAdquirida como opção de recompensa diária\nDe 200 em 200'
-+ ' dias a partir do 300\n**Exemplo**:\n*[300, 500, 700, 900...]*';
-const acquisitionField = (value, inline = false) => (embed) => embed.addField('Aquisição', value, inline);
+  + ' dias a partir do 300\n**Exemplo**:\n*[300, 500, 700, 900...]*';
 
-module.exports = new Map([
+function acquisitionField(value: string, inline = false) {
+  return (embed: MessageEmbed) => embed.addField('Aquisição', value, inline);
+}
+
+export default new Map([
   ['Athodai', acquisitionField('TennoCon 2020\nAssistindo a live por 30 minutos na Twitch')],
   ['Azima', acquisitionField(loginWeapon)],
   ['Zenistar', acquisitionField(loginWeapon)],
