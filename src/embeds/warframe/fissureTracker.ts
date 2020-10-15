f3a193eebb3a4084309ce3ee1ef98fe5abc4fb65
WarNote <andre_20113meira@hotmail.com>
2020-10-15 14:25:42 -0300

    Updated to typescript

--- src/embeds/warframe/fissureTracker.ts
+++ src/embeds/warframe/fissureTracker.ts
@@ -1,7 +1,20 @@
-const { MessageEmbed } = require('discord.js');
-const { groupBy, eachSeries } = require('async');
-const moment = require('moment-timezone');
-const _ = require('lodash');
+import { MessageEmbed } from 'discord.js';
+import { eachSeries } from 'async';
+import moment from 'moment-timezone';
+import _ from 'lodash';
+
+interface Fissure {
+  node: string;
+  expired: boolean;
+  eta: string;
+  missionType: string;
+  tier: string;
+  tierNum: number;
+  enemy: string;
+  id: string;
+  expiry: string;
+  activation: string;
+}
 
 const { timezone } = require('../../config').config;
 // Fissures: Lith, Meso, Neo, Axi and Requiem
@@ -12,16 +25,22 @@ const fissureIcons = [
   'https://i.imgur.com/sk2WIeA.png',
   'https://i.imgur.com/CNdPs70.png',
 ];
+
 const fissureTiers = new Map(['Lith', 'Meso', 'Neo', 'Axi', 'Requiem']
   .map((tier, index) => [tier, { index, icon: fissureIcons[index] }]));
-const fissureToString = ({ node }) => {
+
+function fissureToString({ node }: Fissure) {
   const [mission, map] = [node.split(' ')[0], node.split(' ').slice(1).join(' ')];
   // const expirationString = moment.tz(expiry, timezone).format('HH:mm');
   return `***${map.replace(/[()]/g, '')}***    *${mission}*\n`;
 };
-const missionFissuresToString = (missionFissures) => (
-  missionFissures.reduce((nodes, missionFissure) => `${nodes}${fissureToString(missionFissure)}`, ''));
-const getMissionsFields = (missionFissures) => {
+
+function missionFissuresToString(missionFissures: Fissure[]) {
+  return missionFissures
+    .reduce((nodes, missionFissure) => `${nodes}${fissureToString(missionFissure)}`, '');
+}
+
+function getMissionsFields(missionFissures: Fissure[]) {
   const expirationString = missionFissures.reduce((expirations, { expiry }) => {
     const expiration = moment.tz(expiry, timezone).format('HH:mm');
     return `${expirations}*${expiration}*\n`;
@@ -42,28 +61,32 @@ const getMissionsFields = (missionFissures) => {
   }];
 };
 
-const fissuresEmbed = async (fissures) => {
+export default async function fissuresEmbed(fissures: Fissure[]) {
   const fissuresMap = new Map(
-    [...Object.entries((await groupBy(fissures, async (fissure) => fissure.tier)))]
+    [...Object.entries((_.groupBy(fissures, 'tier')))]
       .sort(([tierA], [tierB]) => {
-        const [{ index: A }, { index: B }] = [fissureTiers.get(tierB), fissureTiers.get(tierA)];
-        return A - B;
+        const { index: A = 0 } = fissureTiers.get(tierA) || {};
+        const { index: B = 0 } = fissureTiers.get(tierB) || {};
+        return B - A;
       }),
   );
   const embedsMap = new Map();
-  await eachSeries(fissuresMap, async ([tier, tierFissures]) => {
-    const { icon } = fissureTiers.get(tier);
+  fissuresMap.forEach((tierFissures, tier) => {
+    const { icon } = fissureTiers.get(tier) || {};
+
     const missionsTypes = _.uniqBy(tierFissures, 'missionType')
       .reduce((types, { missionType }, index, uniqFissures) => (
         `${types} ${missionType}${index === uniqFissures.length - 1 ? '' : ','}`), '');
-    const groupedByType = await groupBy(tierFissures, async ({ missionType }) => missionType);
+
+    const groupedByType = _.groupBy(tierFissures, 'missionType');
     const fields = Object.values(groupedByType)
       .flatMap(([...missionFissures]) => getMissionsFields(missionFissures));
+
     const embed = new MessageEmbed()
       .setTitle(`${tier} fendas ativas: ${tierFissures.length}`)
       .setAuthor(missionsTypes)
       .setDescription('')
-      .setThumbnail(icon)
+      .setThumbnail(icon || '')
       .setTimestamp()
       .setFooter('Horário de São Paulo')
       .addFields(...fields);
@@ -71,5 +94,3 @@ const fissuresEmbed = async (fissures) => {
   });
   return embedsMap;
 };
-
-module.exports = fissuresEmbed;
