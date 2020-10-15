d196d824dcfb2ad65ee239586859021030509618
WarNote <andre_20113meira@hotmail.com>
2020-10-15 18:19:57 -0300

    Updated to typescript

--- src/events/botRecording/botRecording.ts
+++ src/events/botRecording/botRecording.ts
@@ -1,10 +1,12 @@
-const { Event } = require('klasa');
-const AudioRecorder = require('../../customExtendables/audioRecorder');
+import { Event } from 'klasa'
+import type { EventStore } from 'klasa';
+import type { VoiceConnection, VoiceState } from 'discord.js';
+import AudioRecorder from '../../customExtendables/audioRecorder'
 
 const recorderMap = new Map();
 
-module.exports = class extends Event {
-  constructor(...args) {
+export default class extends Event {
+  constructor(...args: [EventStore, string[], string]) {
     super(...args, {
       event: 'botJoinedChannel',
       enabled: true,
@@ -12,13 +14,14 @@ module.exports = class extends Event {
     });
   }
 
-  async run({ state }) {
+  async run({ state }: { state: VoiceState }) {
     if (recorderMap.has(state.guild.id)) {
       const oldAudioRecorder = recorderMap.get(state.guild.id);
       await oldAudioRecorder.stopRecording();
       recorderMap.delete(state.guild.id);
     }
-    const newRecorder = new AudioRecorder(state.connection, this.client);
+
+    const newRecorder = new AudioRecorder(state.connection as VoiceConnection, this.client);
     recorderMap.set(state.guild.id, newRecorder);
     await newRecorder.startRecording();
   }
