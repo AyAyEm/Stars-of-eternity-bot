0e50867fba2097332c67884b72010bf9a2bc81c5
WarNote <andre_20113meira@hotmail.com>
2020-10-15 18:57:41 -0300

    Added fissure types

---
+++ src/types/WFCD/fissure.d.ts
@@ -0,0 +1,12 @@
+export interface Fissure {
+  node: string;
+  expired: boolean;
+  eta: string;
+  missionType: string;
+  tier: string;
+  tierNum: number;
+  enemy: string;
+  id: string;
+  expiry: string;
+  activation: string;
+}
