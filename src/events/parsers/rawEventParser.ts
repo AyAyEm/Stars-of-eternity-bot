be6eec92cb29cb95c65322c7723afd08ae8445b0
WarNote <andre_20113meira@hotmail.com>
2020-10-15 18:36:21 -0300

    Updated to typescript

--- src/events/parsers/rawEventParser.ts
+++ src/events/parsers/rawEventParser.ts
@@ -1,8 +1,32 @@
-const { Event } = require('klasa');
-const { MessageReaction } = require('discord.js');
+import { Event } from 'klasa'
+import { MessageReaction } from 'discord.js'
 
-module.exports = class extends Event {
-  constructor(...args) {
+import type { TextChannel, User } from 'discord.js';
+import type { EventStore } from 'klasa';
+
+interface RawPacket {
+  t: string;
+  s: number;
+  op: number;
+  d: D;
+}
+
+interface D {
+  user_id: string;
+  message_id: string;
+  emoji: Emoji;
+  channel_id: string;
+  guild_id: string;
+}
+
+interface Emoji {
+  name: string;
+  id: null;
+  animated: boolean;
+}
+
+export default class extends Event {
+  constructor(...args: [EventStore, string[], string]) {
     super(...args, {
       enabled: true,
       once: false,
@@ -10,16 +34,15 @@ module.exports = class extends Event {
     });
   }
 
-  async run(packet) {
-    const { client } = this;
+  async run(packet: RawPacket) {
     // We don't want this to run on unrelated packets
     if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
     // Grab the channel to check the message from
-    const channel = await client.channels.fetch(packet.d.channel_id, true);
+    const channel = await this.client.channels.fetch(packet.d.channel_id, true);
     // There's no need to emit if the message is cached, because the event will fire anyway for that
-    if (channel.messages.cache.has(packet.d.message_id)) return;
+    if ((channel as TextChannel).messages.cache.has(packet.d.message_id)) return;
     // Since we have confirmed the message is not cached, let's fetch it
-    channel.messages.fetch(packet.d.message_id).then((message) => {
+    (channel as TextChannel).messages.fetch(packet.d.message_id).then((message) => {
       // Emojis can have identifiers of name:id format, so we have to account for that case as well
       const emoji = packet.d.emoji.id ? packet.d.emoji.id : packet.d.emoji.name;
       // This gives us the reaction we need to emit the event properly, in top of the message object
@@ -29,14 +52,14 @@ module.exports = class extends Event {
         },
       }, message);
       // Adds the currently reacting user to the reaction's users collection.
-      const user = client.users.resolve(packet.d.user_id);
-      if (reaction) reaction.users.cache.set(packet.d.user_id, user);
+      const user = this.client.users.resolve(packet.d.user_id);
+      if (reaction) reaction.users.cache.set(packet.d.user_id, user as User);
       // Check which type of event it is before emitting
       if (packet.t === 'MESSAGE_REACTION_ADD') {
-        client.emit('messageReactionAdd', reaction, user);
+        (this.client as any).emit('messageReactionAdd', reaction, user);
       }
       if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-        client.emit('messageReactionRemove', reaction, user);
+        (this.client as any).emit('messageReactionRemove', reaction, user);
       }
     });
   }
