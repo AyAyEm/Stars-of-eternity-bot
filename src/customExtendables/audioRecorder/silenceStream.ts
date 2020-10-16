d7cb8a6d1dca8ca79a29f29b97ec80b6259fc31e
WarNote <andre_20113meira@hotmail.com>
2020-10-16 16:28:56 -0300

    Disabled eslint rule

--- src/customExtendables/audioRecorder/silenceStream.ts
+++ src/customExtendables/audioRecorder/silenceStream.ts
@@ -2,6 +2,7 @@ import { Readable } from 'stream';
 
 const silenceFrame = Buffer.from([0xF8, 0xFF, 0xFE]);
 export class SilenceStream extends Readable {
+  // eslint-disable-next-line no-underscore-dangle
   _read() {
     this.push(silenceFrame);
   }
