faf4131a7ff54fa12960c78a736e666eb7d77038
WarNote <andre_20113meira@hotmail.com>
2020-10-16 15:17:25 -0300

    Formated a bit and added a provider extension

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
-require('dotenv').config();
+import { Client, PermissionLevels } from 'klasa';
+import { config, token } from './config';
 
-const { Client, PermissionLevels } = require('klasa');
-const { config, token } = require('./config');
+import dotenv from 'dotenv';
+dotenv.config();
 
 class MyKlasaClient extends Client {
   // eslint-disable-next-line no-useless-constructor
   constructor(...args: any) {
     super(...args);
-    // Add any properties to your Klasa Client
   }
-  // Add any methods to your Klasa Client
+
+  get provider() {
+    return this.providers.get('mongoose');
+  }
 }
 
-config.permissionLevels = new PermissionLevels()
-  .add(0, () => true)
-  .add(5, ({ guild, member }: any) => guild && member.permissions.has('MANAGE_GUILD'), { break: true, fetch: true })
-  .add(6, ({ guild, member }: any) => guild && member.permissions.has('ADMINISTRATOR'), { break: true, fetch: true })
-  .add(7, ({ guild, member }: any) => guild && member === guild.owner, { break: true, fetch: true })
-  .add(9, ({ author, client }: any) => client.owners.has(author), { break: true })
-  .add(10, ({ author, client }: any) => client.owners.has(author));
-// Allows the bot owner to use Bot Owner only commands, which silently fail for other users.
+interface Config {
+
+}
+
+Object.assign(config, {
+  permissionLevels: new PermissionLevels()
+    .add(0, () => true)
+    .add(5, ({ guild, member }: any) => guild && member.permissions.has('MANAGE_GUILD'), { break: true, fetch: true })
+    .add(6, ({ guild, member }: any) => guild && member.permissions.has('ADMINISTRATOR'), { break: true, fetch: true })
+    .add(7, ({ guild, member }: any) => guild && member === guild.owner, { break: true, fetch: true })
+    .add(9, ({ author, client }: any) => client.owners.has(author), { break: true })
+    .add(10, ({ author, client }: any) => client.owners.has(author)),
+});
 
 new MyKlasaClient(config).login(token);
