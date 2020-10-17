9b7cb869c8cfc69601bce0c20186b48b81ab6c9d
WarNote <andre_20113meira@hotmail.com>
2020-10-17 20:26:09 -0300

    Updated to typescript

--- src/commands/bot admin/guildEmojis.ts
+++ src/commands/bot admin/guildEmojis.ts
@@ -1,8 +1,11 @@
-const { Command } = require('klasa');
-const staticGuildEmojis = require('../../static/guildEmojis');
+import { Command } from 'klasa';
 
-module.exports = class extends Command {
-  constructor(...args) {
+import type { CommandStore, KlasaMessage } from 'klasa';
+
+import { guildEmojis } from '../../static';
+
+export default class extends Command {
+  constructor(...args: [CommandStore, string[], string]) {
     super(...args, {
       enabled: true,
       runIn: ['text'],
@@ -16,23 +19,24 @@ module.exports = class extends Command {
     });
   }
 
-  async setStore(msg) {
+  async setStore(msg: KlasaMessage) {
     const { guild } = msg;
-    const emojiUtil = await this.client.provider.Util('Emojis');
+    const provider: any = this.client.providers.get('mongoose');
+    const emojiUtil = await provider.Util('Emojis');
     const { document } = emojiUtil;
-    const onlyEmojis = [...staticGuildEmojis.values()]
+    const onlyEmojis = [...guildEmojis.values()]
       .flatMap((emoji) => Object.values(emoji));
     const emojisMap = document.emojis || new Map();
     let needToUpdate = false;
     const loopPromises = onlyEmojis.map(async ({ name, image }) => {
-      const cacheEmojiID = guild.emojis.cache.findKey((emoji) => emoji.name === name);
+      const cacheEmojiID = guild?.emojis.cache.findKey((emoji) => emoji.name === name);
       if (emojisMap.has(name) && emojisMap.get(name).id === cacheEmojiID) return;
       needToUpdate = true;
       if (cacheEmojiID) {
-        emojisMap.set(name, { id: cacheEmojiID, guild: guild.id });
+        emojisMap.set(name, { id: cacheEmojiID, guild: guild?.id });
       } else {
-        const newEmoji = await guild.emojis.create(image, name, { reason: 'bot needed emoji' });
-        emojisMap.set(name, { id: newEmoji.id, guild: guild.id });
+        const newEmoji = await guild?.emojis.create(image, name, { reason: 'bot needed emoji' });
+        emojisMap.set(name, { id: newEmoji?.id, guild: guild?.id });
       }
     });
     await Promise.all(loopPromises);
@@ -40,4 +44,4 @@ module.exports = class extends Command {
     document.emojis = new Map([...emojisMap.entries()].sort());
     document.save();
   }
-};
+}
