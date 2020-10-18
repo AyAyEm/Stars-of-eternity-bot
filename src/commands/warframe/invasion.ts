083e5b8785399920e00f11b962c2e192cf891e6a
WarNote <andre_20113meira@hotmail.com>
2020-10-17 21:50:04 -0300

    Updated to typescript

--- src/commands/warframe/invasion.ts
+++ src/commands/warframe/invasion.ts
@@ -1,5 +1,9 @@
-const { Command } = require('klasa');
-const invasionItemsEmbed = require('../../embeds/warframe/invasionItems');
+import { Command } from 'klasa';
+
+import type { Channel } from 'discord.js';
+import type { CommandStore, KlasaGuild, KlasaMessage } from 'klasa';
+
+import invasionItemsEmbed from '../../embeds/warframe/invasionItems';
 
 const possibleItems = new Set([
   'vauban', 'vandal', 'wraith', 'skin', 'helmet',
@@ -12,12 +16,12 @@ const possibleItems = new Set([
   'kubrowEgg', 'traces', 'other', 'credits',
 ]);
 
-module.exports = class extends Command {
-  constructor(...args) {
+export default class extends Command {
+  constructor(...args: [CommandStore, string[], string]) {
     super(...args, {
       enabled: true,
       runIn: ['text'],
-      autoaliases: true,
+      autoAliases: true,
       permissionLevel: 6,
       subcommands: true,
       usage: '<items|listItems|add|disable|enable|delete|forceDelete|set> '
@@ -29,7 +33,7 @@ module.exports = class extends Command {
     this
       .createCustomResolver('invasionItem', (arg, _possible, msg) => {
         const semiRequired = new Set(['add', 'set']);
-        const [action] = msg.prompter.args;
+        const [action] = (msg as any).prompter?.args || [];
         if (!semiRequired.has(action)) return arg;
         if (arg && !possibleItems.has(arg)) {
           throw new Error(`Item ${arg} inválido para listar as opções digite /invasion listItems!`);
@@ -38,25 +42,25 @@ module.exports = class extends Command {
       });
   }
 
-  async getDocument(guild) {
-    const provider = await this.client.providers.get('mongoose');
+  async getDocument(guild: KlasaGuild) {
+    const provider: any = this.client.providers.get('mongoose');
     return provider.Guilds.findOne({ id: guild.id });
   }
 
-  async updateItems(guildDocument, channel, items) {
+  async updateItems(guildDocument: any, channel: Channel, items: string[]) {
     const path = `channels.${channel.id}.invasionItems.items`;
     return guildDocument.updateOne({ [path]: items });
   }
 
-  async updateStatus(guildDocument, channel, status) {
+  async updateStatus(guildDocument: any, channel: Channel, status: boolean) {
     const path = `channels.${channel.id}.invasionItems.enabled`;
     return guildDocument.updateOne({ [path]: status });
   }
 
-  async set(msg, [items = [...possibleItems.values()]]) {
+  async set(msg: KlasaMessage, [items = [...possibleItems.values()]]) {
     const { channel, guild } = msg;
     const { updateItems, updateStatus } = this;
-    const guildDocument = await this.getDocument(guild);
+    const guildDocument = await this.getDocument(guild as KlasaGuild);
     if (guildDocument.get(`channels.${channel.id}.invasionItems`)) {
       throw new Error('Canal já está registrado!');
     }
@@ -65,76 +69,76 @@ module.exports = class extends Command {
       updateStatus(guildDocument, channel, true),
     ])
       .then(() => {
-        msg.replyAndDelete('Canal inscrito nas invasões com sucesso! Items:'
+        (msg as any).replyAndDelete('Canal inscrito nas invasões com sucesso! Items:'
           + `\`\`\`${items.join(' | ')}\`\`\``);
       });
   }
 
-  async add(msg, [...items]) {
+  async add(msg: KlasaMessage, [...items]) {
     const { channel, guild } = msg;
     const { updateItems } = this;
-    const guildDocument = await this.getDocument(guild);
+    const guildDocument = await this.getDocument(guild as KlasaGuild);
     const invasionItems = guildDocument.get(`channels.${channel.id}.invasionItems`);
     if (!invasionItems) throw new Error('Este canal não está configurado para invasões!');
     if (items.length === 0) throw new Error('Items precisam ser específicados!');
     const updatedItems = items[0] === 'all' ? possibleItems : items;
     const itemsData = new Set([...invasionItems.items, ...updatedItems]);
     await updateItems(guildDocument, channel, [...itemsData.values()]);
-    msg.replyAndDelete(`Items inscritos com sucesso! Items: \`${items}\``);
+    (msg as any).replyAndDelete(`Items inscritos com sucesso! Items: \`${items}\``);
   }
 
-  async disable(msg) {
+  async disableFn(msg: KlasaMessage) {
     const { channel, guild } = msg;
     const { updateStatus } = this;
-    const guildDocument = await this.getDocument(guild);
+    const guildDocument = await this.getDocument(guild as KlasaGuild);
     const invasionItems = guildDocument.get(`channels.${msg.channel.id}.invasionItems`) || {};
     if (invasionItems.enabled === false) {
       throw new Error('Invasões já estão desabilitadas para este canal!');
     }
     await updateStatus(guildDocument, channel, false);
-    msg.replyAndDelete('Invasões desabilitadas com sucesso!');
+    (msg as any).replyAndDelete('Invasões desabilitadas com sucesso!');
   }
 
-  async enable(msg) {
+  async enableFn(msg: KlasaMessage) {
     const { channel, guild } = msg;
     const { updateStatus } = this;
-    const guildDocument = await this.getDocument(guild);
+    const guildDocument = await this.getDocument(guild as KlasaGuild);
     const invasionItems = guildDocument.get(`channels.${msg.channel.id}.invasionItems`) || {};
     if (invasionItems.enabled === true) {
       throw new Error('Invasões já estão habilitadas para este canal!');
     }
     await updateStatus(guildDocument, channel, true);
-    msg.replyAndDelete('Invasões habilitadas com sucesso!');
+    (msg as any).replyAndDelete('Invasões habilitadas com sucesso!');
   }
 
-  async delete(msg, [...items]) {
+  async delete(msg: KlasaMessage, [...items]) {
     const { guild, channel } = msg;
-    const guildDocument = await this.getDocument(guild);
+    const guildDocument = await this.getDocument(guild as KlasaGuild);
     const itemsPath = `channels.${channel.id}.invasionItems.items`;
     const invasionItems = guildDocument.get(itemsPath) || [];
     if (invasionItems.length === 0) {
       throw new Error('Os items de invasão ainda não foram definidos para este canal!');
     }
     const updatedItems = items.length > 0
-      ? invasionItems.filter((invasionItem) => !items.includes(invasionItem))
+      ? invasionItems.filter((invasionItem: string) => !items.includes(invasionItem))
       : [];
     await guildDocument.updateOne({ [itemsPath]: updatedItems });
-    msg.replyAndDelete('Items de invasão excluídos com sucesso!'
-      + `\`\`\`${invasionItems.filter((invasionItem) => !updatedItems.includes(invasionItem)).join(' | ')}\`\`\``);
+    (msg as any).replyAndDelete('Items de invasão excluídos com sucesso!'
+      + `\`\`\`${invasionItems.filter((invasionItem: string) => !updatedItems.includes(invasionItem)).join(' | ')}\`\`\``);
   }
 
-  async forceDelete(msg) {
+  async forceDelete(msg: KlasaMessage) {
     const { channel, guild } = msg;
-    const guildDocument = await this.getDocument(guild);
+    const guildDocument = await this.getDocument(guild as KlasaGuild);
     const channelPath = `channels.${channel.id}`;
     const channelDocument = guildDocument.get(channelPath).toObject();
     delete channelDocument.invasionItems;
     await guildDocument.updateOne({ [channelPath]: channelDocument });
-    msg.replyAndDelete('Items de invasão removidos do database com sucesso!');
+    (msg as any).replyAndDelete('Items de invasão removidos do database com sucesso!');
   }
 
-  async items(msg) {
-    const guildDocument = await this.getDocument(msg.guild);
+  async items(msg: KlasaMessage) {
+    const guildDocument = await this.getDocument(msg.guild as KlasaGuild);
     const items = guildDocument.get(`channels.${msg.channel.id}.invasionItems.items`);
     if (!items || items.length === 0) {
       throw new Error('Nenhum item de invasão encontrado para este canal!');
@@ -142,7 +146,7 @@ module.exports = class extends Command {
     msg.channel.send(`\`\`\`Items encontrados: \n${items.join(' | ')}\`\`\``);
   }
 
-  async listItems(msg) {
+  async listItems(msg: KlasaMessage) {
     msg.channel.send(invasionItemsEmbed);
   }
-};
+}
