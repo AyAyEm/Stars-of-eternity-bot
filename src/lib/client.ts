a80fa95b82284e3011c8a81402628cf7a9431057
WarNote <andre_20113meira@hotmail.com>
2020-10-23 19:29:41 -0300

    Added required methods to client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,14 @@
 import { SapphireClient } from '@sapphire/framework';
+import { ClientOptions } from 'discord.js';
 import { Mongoose } from './providers';
 
 export class EternityClient extends SapphireClient {
   public provider: Mongoose = new Mongoose();
 
   public fetchPrefix = () => '/';
+
+  constructor(options?: ClientOptions) {
+    super(options);
+    this.registerUserDirectories();
+  }
 }
