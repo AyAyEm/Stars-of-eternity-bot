102d50d302273c69596a3e2655ffdaa2d7422d3d
WarNote <andre_20113meira@hotmail.com>
2020-10-25 11:12:53 -0300

    Added ownersIds array

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 
 const env = dotenv.config().parsed;
 export const config = {
+  ownersIds: [''],
   token: env.DISCORD_TOKEN || '',
   mongoConnectionString: env.MONGO_CONNECTION_STRING || '',
 };
