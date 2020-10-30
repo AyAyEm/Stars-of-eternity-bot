08258b6bd0de575b5e316c6ed728058d4439a3b2
WarNote <andre_20113meira@hotmail.com>
2020-10-30 15:17:15 -0300

    Warframe related types

---
+++ src/lib/types/Warframe.d.ts
@@ -0,0 +1,48 @@
+import type { Item } from 'warframe-items';
+
+export interface Fissure {
+  node: string;
+  expired: boolean;
+  eta: string;
+  missionType: string;
+  tier: string;
+  tierNum: number;
+  enemy: string;
+  id: string;
+  expiry: string;
+  activation: string;
+  active: boolean;
+}
+
+export interface InvasionData {
+  defenderReward: Reward;
+  attackingFaction: string;
+  completion: number;
+  attackerReward: Reward;
+  count: number;
+  completed: boolean;
+  requiredRuns: number;
+  vsInfestation: boolean;
+  node: string;
+  eta: string;
+  defendingFaction: string;
+  id: string;
+  activation: Date;
+  rewardTypes: string[];
+  desc: string;
+}
+
+export interface Reward {
+  countedItems: CountedItem[];
+  thumbnail: string;
+  color: number;
+  credits: number;
+  asString: string;
+  items: Item[];
+  itemString: string;
+}
+
+export interface CountedItem {
+  count: number;
+  type: string;
+}
