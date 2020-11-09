c599b89bb619a23367994faf992bcd2cebef02b7
WarNote <andre_20113meira@hotmail.com>
2020-11-09 10:00:58 -0300

    Guild extension

--- src/lib/extensions/EternityGuild.ts
+++ src/lib/extensions/EternityGuild.ts
@@ -1,7 +1,25 @@
 import { Structures } from 'discord.js';
+import { EternityVoiceChannel } from '@lib';
+
+import type { GuildMember, Collection } from 'discord.js';
 
 export class EternityGuild extends Structures.get('Guild') {
   get voiceChannels() {
     return this.channels.cache.filter(({ type }) => type === 'voice');
+  }
+
+  get channelWithMostMembers(): EternityVoiceChannel {
+    const voiceChannels = this.channels.cache
+      .filter((channel) => channel.type === 'voice') as Collection<string, EternityVoiceChannel>;
+
+    return voiceChannels
+      .map((channel) => {
+        const userCount = channel.members
+          .filter((member: GuildMember) => !member.user.bot)
+          .keyArray().length;
+
+        return { userCount, channel };
+      })
+      .sort(({ userCount: A }, { userCount: B }) => B - A)[0].channel;
   }
 }
