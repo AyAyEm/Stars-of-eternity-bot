823acb02455376b6552f1e260b33dbc1cfb1a05b
WarNote <andre_20113meira@hotmail.com>
2020-11-09 10:01:13 -0300

    TextChannel extension

--- src/lib/extensions/EternityTextChannel.ts
+++ src/lib/extensions/EternityTextChannel.ts
@@ -1,5 +1,11 @@
 import { Structures } from 'discord.js';
 
+import type { EternityGuild } from './EternityGuild';
+
+export interface EternityTextChannel {
+  guild: EternityGuild;
+}
+
 export class EternityTextChannel extends Structures.get('TextChannel') {
   public async sendAndDelete(content: string, { timeout = 10000, reason = '' }) {
     return (await this.send(content)).delete({ timeout, reason });
