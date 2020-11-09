668624ce38d09e0c358c4d6cfb88839f0acdb092
WarNote <andre_20113meira@hotmail.com>
2020-11-09 10:01:22 -0300

    VoiceChannel extension

--- src/lib/extensions/EternityVoiceChannel.ts
+++ src/lib/extensions/EternityVoiceChannel.ts
@@ -1,5 +1,11 @@
 import { Structures } from 'discord.js';
 
+import type { EternityGuild } from './EternityGuild';
+
+export interface EternityVoiceChannel {
+  guild: EternityGuild;
+}
+
 export class EternityVoiceChannel extends Structures.get('VoiceChannel') {
   get onlyMembers() {
     return this.members.filter((guildMember) => !guildMember.user.bot);
