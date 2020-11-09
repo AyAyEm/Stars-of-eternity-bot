86d2d8e39d0e1af24ff49cb7d29e0a1981f72109
WarNote <andre_20113meira@hotmail.com>
2020-11-09 09:48:27 -0300

    Reduced the code

--- src/tasks/InitialTasks.ts
+++ src/tasks/InitialTasks.ts
@@ -8,26 +8,25 @@ import type { Guild } from 'discord.js';
 export default class extends Task {
   public async run() {
     // Cache active bot voice connections
-    const clientId = this.client.user?.id || '';
-    await new Promise((resolve) => {
-      this.client.guilds.cache.each(async (guild: Guild) => {
-        const voiceChannels = guild.channels.cache.filter(({ type }) => type === 'voice');
-        await voiceChannels.each(async (voiceChannel) => {
-          const { members } = voiceChannel;
-          if (!members.has(clientId)) return;
-          const guildMember = await members.get(clientId)?.fetch();
-          const clientVoiceConnection = guildMember?.voice.connection;
-          // If the client is in a channel and doesn't have a connection we'll kick it.
-          if (!clientVoiceConnection) {
-            guildMember?.voice.kick('Server restart auto bot kick');
-            return;
-          }
-          const snowflake = SnowflakeUtil.generate();
-          this.client.voice?.connections.set(snowflake, clientVoiceConnection);
-        });
-        resolve();
+    const clientId = this.client.id || '';
+    await new Promise((resolve) => this.client.guilds.cache.each(async (guild: Guild) => {
+      const voiceChannels = guild.channels.cache.filter(({ type }) => type === 'voice');
+      await voiceChannels.each(async (voiceChannel) => {
+        const { members } = voiceChannel;
+        if (!members.has(clientId)) return;
+        const guildMember = await members.get(clientId)?.fetch();
+        const clientVoiceConnection = guildMember?.voice.connection;
+
+        // If the client is in a channel and doesn't have a connection we'll kick it.
+        if (!clientVoiceConnection) {
+          guildMember?.voice.kick('Server restart auto bot kick');
+          return;
+        }
+        const snowflake = SnowflakeUtil.generate();
+        this.client.voice?.connections.set(snowflake, clientVoiceConnection);
       });
-    });
+      resolve();
+    }));
     this.client.voice?.connections.each((voiceConnection) => voiceConnection.disconnect());
     setTimeout(() => this.client.emit('initialTasksCompleted'), 500);
   }
