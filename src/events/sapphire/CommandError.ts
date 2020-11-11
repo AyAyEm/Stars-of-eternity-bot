5b2fd1f6931fc15e119a1d21112ca4198d3b80cf
WarNote <andre_20113meira@hotmail.com>
2020-11-11 19:33:41 -0300

    Refactored: using instanceof operator

--- src/events/sapphire/CommandError.ts
+++ src/events/sapphire/CommandError.ts
@@ -1,19 +1,18 @@
-import { EternityEvent } from '@lib';
+import { EternityEvent, CommandError } from '@lib';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events } from '@sapphire/framework';
 
 import type { EventOptions, CommandErrorPayload } from '@sapphire/framework';
-import type { CommandError } from '@lib/errors';
 
 type PossibleErrors = Error | CommandError | unknown;
 
 @ApplyOptions<EventOptions>({ event: Events.CommandError })
 export default class extends EternityEvent<Events.CommandError> {
-  public run(error: PossibleErrors, { message }: CommandErrorPayload) {
-    if (typeof error !== 'object') return;
-    if ((error as CommandError).name === 'CommandError') {
-      const { langPath } = error as CommandError;
-      message.sendTranslated(langPath);
+  public run(error: PossibleErrors, { message, piece }: CommandErrorPayload) {
+    if (error instanceof CommandError) {
+      message.sendTranslated(`commands:${piece.name}.${error.identifier}`);
+    } else {
+      this.client.console.error(error);
     }
   }
 }
