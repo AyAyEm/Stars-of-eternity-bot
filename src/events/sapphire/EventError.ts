b68bdaef9e2b13820bb46d804f3dad57fe5edb78
WarNote <andre_20113meira@hotmail.com>
2020-11-11 19:35:38 -0300

    Feat: now handling missingArgument error

--- src/events/sapphire/EventError.ts
+++ src/events/sapphire/EventError.ts
@@ -1,12 +1,19 @@
-import { EternityEvent } from '@lib';
-import { Events } from '@sapphire/framework';
+import { EternityEvent, CommandError } from '@lib';
+import { Events, ArgumentError } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 
 import type { EventOptions } from '@sapphire/framework';
 
+type PossibleErrors = ArgumentError<unknown> | CommandError | Error;
+
 @ApplyOptions<EventOptions>({ event: Events.EventError })
 export default class extends EternityEvent<Events.EventError> {
-  public async run(err: Error) {
-    console.error(err);
+  public async run(error: PossibleErrors) {
+    if (error instanceof CommandError && error.identifier === 'missingArgument') return;
+    if (error instanceof ArgumentError) {
+      this.client.console.error(error);
+    } else {
+      this.client.console.log(error);
+    }
   }
 }
