084b388a67813da153ae1f6567b42f60f801c014
WarNote <andre_20113meira@hotmail.com>
2020-11-20 08:40:29 -0300

    Refactor: regex typesafe

--- src/arguments/discord/Emoji.ts
+++ src/arguments/discord/Emoji.ts
@@ -9,9 +9,7 @@ export class Emoji extends Argument<DiscordEmoji> {
   public unicodeEmojiRegex = /u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]/;
 
   public async run(argument: string, context: ArgumentContext) {
-    const unicodeEmoji = this.unicodeEmojiRegex.test(argument)
-      ? argument.match(this.unicodeEmojiRegex)[0]
-      : null;
+    const unicodeEmoji = argument.match(this.unicodeEmojiRegex)?.[0];
 
     if (unicodeEmoji) {
       return this.ok(new DiscordEmoji(this.client, {
@@ -22,7 +20,7 @@ export class Emoji extends Argument<DiscordEmoji> {
       }));
     }
 
-    const id = argument.match(/\d+/)[0];
+    const id = argument.match(/\d+/)?.[0];
     const guildEmoji = context.message.guild.emojis.resolve(id) || null;
     if (guildEmoji) return this.ok(guildEmoji);
     const defaultErrorMessage = 'Argument passed cannot resolve to a valid emoji';
