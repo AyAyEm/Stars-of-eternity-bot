c252fd8e6958ef0174a444c706b7174cac7cb7dc
WarNote <andre_20113meira@hotmail.com>
2020-11-20 13:28:39 -0300

    Refactor: Any CommandError now it'll be ignored

--- src/events/sapphire/EventError.ts
+++ src/events/sapphire/EventError.ts
@@ -9,7 +9,7 @@ type PossibleErrors = ArgumentError<unknown> | CommandError | Error;
 @ApplyOptions<EventOptions>({ event: Events.EventError })
 export default class extends EternityEvent<Events.EventError> {
   public async run(error: PossibleErrors) {
-    if (error instanceof CommandError && error.identifier === 'missingArgument') return;
+    if (error instanceof CommandError) return;
     if (error instanceof ArgumentError) {
       this.client.console.error(error);
     } else {
