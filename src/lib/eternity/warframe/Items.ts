3bcac69be72e2d3f84c8d23914dd17f23d16a6ad
WarNote <andre_20113meira@hotmail.com>
2020-11-20 09:00:44 -0300

    Refactor: change uniqueNameDict to a getter

--- src/lib/eternity/warframe/Items.ts
+++ src/lib/eternity/warframe/Items.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import * as _ from 'lodash';
-import * as async from 'async';
-import * as fse from 'fs-extra';
-import * as path from 'path';
+import _ from 'lodash';
+import async from 'async';
+import fse from 'fs-extra';
+import path from 'path';
 
 import type { Item } from 'warframe-items';
+import type { Awaited } from '@sapphire/framework';
 
 import Root = require('app-root-path');
 
@@ -13,9 +14,21 @@ export class Items {
 
   public readonly source = 'https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/All.json';
 
-  public uniqueNameDict?: Record<string, string>;
+  private _uniqueNameDict: Record<string, string>;
 
   public latestUpdate?: Date;
+
+  get uniqueNameDict(): Awaited<Record<string, string>> {
+    if (!this._uniqueNameDict) {
+      const result = import(`${this.dir}.json`);
+      result.then((uniqueNameDict: Record<string, string>) => {
+        this._uniqueNameDict = uniqueNameDict;
+      });
+
+      return result;
+    }
+    return this._uniqueNameDict;
+  }
 
   public async create() {
     const { data }: { data: Item[] } = await axios.get(this.source);
@@ -23,7 +36,7 @@ export class Items {
     const dataDict = _.fromPairs(_.map(data, ({ name, uniqueName }) => (
       [name.toLowerCase(), uniqueName])));
 
-    this.uniqueNameDict = dataDict;
+    this._uniqueNameDict = dataDict;
     fse.outputJson(`${this.dir}.json`, dataDict);
 
     const operations = data.map((item) => async () => (
@@ -34,11 +47,9 @@ export class Items {
   }
 
   public async get(name: string, createIfNotExists = false): Promise<Item | null> {
-    const uniqueNameDict = this.uniqueNameDict || await import(`${this.dir}.json`);
+    const uniqueNameDict = await this.uniqueNameDict;
     if (!this.uniqueNameDict && createIfNotExists) await this.create();
     else if (!uniqueNameDict && !createIfNotExists) return null;
-
-    if (!this.uniqueNameDict && !!uniqueNameDict) this.uniqueNameDict = uniqueNameDict;
 
     return import(path.join(this.dir, uniqueNameDict[name.toLowerCase()]));
   }
