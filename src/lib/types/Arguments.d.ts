cdc57a0dc2e0b1b6ac26204c930a481e970d1166
WarNote <andre_20113meira@hotmail.com>
2020-11-23 05:04:26 -0300

    Feat: warframeItem Argument

--- src/lib/types/Arguments.d.ts
+++ src/lib/types/Arguments.d.ts
@@ -1,7 +1,9 @@
 import type { Emoji } from '@root/arguments/discord/Emoji';
+import type { Item } from '@root/arguments/warframe/Item';
 
 declare module '@sapphire/framework' {
   interface ArgType {
     emoji: Emoji;
+    warframeItem: Item;
   }
 }
