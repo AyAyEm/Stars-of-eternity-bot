1d60980c37e5e2e4a0259d882c98023693537c40
WarNote <andre_20113meira@hotmail.com>
2020-12-04 16:20:08 -0300

    Fix: warframeItem type

--- src/lib/types/Arguments.d.ts
+++ src/lib/types/Arguments.d.ts
@@ -1,9 +1,8 @@
 import type { Emoji } from '@root/arguments/discord/Emoji';
-import type { Item } from '@root/arguments/warframe/Item';
 
 declare module '@sapphire/framework' {
   interface ArgType {
     emoji: Emoji;
-    warframeItem: Item;
+    warframeItem: string;
   }
 }
